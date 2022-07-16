2994e9785e1f5157e0f1ece25e97f5ba7cb47570
priambodo.pangestu <priambodo.pangestu@primecash.co.id>
2022-07-16 17:25:35 +0700

    add landing screen for quiz app

--- Rap Story/ContentView.swift
+++ Rap Story/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 8/255, green: 14/255, blue: 44/255);
+    let accentColor = Color(red: 125/255, green: 254/255, blue: 227/255);
+    
+    
     var body: some View {
-        Text("Rap Story")
-            .padding()
+        ZStack{
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("Who are two of the top rappers?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack{
+                    Button(action: {print("Choosed option 1")}, label: {Text("Eric B & Rakim")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)}
+                    )
+                    Button(action: {print("Choosed option 2")}, label: {Text("Pac & Bigg")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)}
+                    )
+                    Button(action: {print("Choosed option 3")}, label: {Text("Andre & Big Boi")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)}
+                    )
+                    Button(action: {print("Choosed option 4")}, label: {Text("Brick & Lace")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)}
+                    )
+                }
+            }
+        }.foregroundColor(.white)
     }
 }
 
