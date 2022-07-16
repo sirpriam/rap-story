46a4ce7549ca83546ff0f41f0735dc48cf959bb9
priambodo.pangestu <priambodo.pangestu@primecash.co.id>
2022-07-16 16:38:11 +0700

    Initial Commit

---
+++ Rap Story/Rap_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rap_StoryApp.swift
+//  Rap Story
+//
+//  Created by Pangestu, Priambodo on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Rap_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
